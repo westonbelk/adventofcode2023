1f3e211c4dfc3b6a24ef8eda0ed58dbbe892c13b
Weston Belk <contact@westonbelk.com>
2023-11-30 23:57:42 -0800

    day 1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/westonbelk/adventofcode
+
+go 1.21.2
+
+require golang.org/x/text v0.14.0
---
+++ util/input.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadLines(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		panic(scanner.Err)
+	}
+	return lines
+}
+
+func Reverse(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, rune := range s {
+		n--
+		runes[n] = rune
+	}
+	return string(runes[n:])
+}
