273f2a025b219d7f4c0080cd7fd43459ec9da26d
Weston Belk <contact@westonbelk.com>
2023-12-04 23:16:19 -0800

    part 1

--- day5/day5.go
+++ day5/day5.go
@@ -2,23 +2,119 @@ package day5
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/westonbelk/adventofcode/util"
 )
 
+type Mapping struct {
+	DestinationRangeStart int
+	SourceRangeStart      int
+	RangeLength           int
+}
+
+func (m Mapping) Contains(n int) bool {
+	return n >= m.SourceRangeStart && n <= m.SourceRangeStart+m.RangeLength
+}
+
+func (m Mapping) Transform(n int) int {
+	diff := n - m.SourceRangeStart
+	return m.DestinationRangeStart + diff
+}
+
 var input []string
 
 func Execute() {
-	// input = util.ReadLines("day4/cali2.txt")
-	input = util.ReadLines("day4/input.txt")
+	// input = util.ReadLines("day5/calibration.txt")
+	input = util.ReadLines("day5/input.txt")
 
-	for i := range input {
-		process(i)
+	seeds := readNums(strings.Fields((input[0]))[1:])
+	maps := mappings()
+
+	fmt.Println("seeds:", seeds)
+
+	locations := make([]int, len(seeds))
+	for seedIdx, _ := range seeds {
+		seed := seeds[seedIdx]
+		for _, kind := range maps {
+			for _, mapping := range kind {
+				if mapping.Contains(seed) {
+					seed = mapping.Transform(seed)
+					break
+				}
+			}
+		}
+		locations[seedIdx] = seed
+		fmt.Printf("Seed number %d corresponds to soil number %d\n", seeds[seedIdx], seed)
 	}
 
-	fmt.Println("total:", 0)
+	lowest := -1
+	for _, l := range locations {
+		if lowest == -1 {
+			lowest = l
+		}
+		if l < lowest {
+			lowest = l
+		}
+	}
+
+	fmt.Println("lowest:", lowest)
 
 }
 
-func process(n int) {
+func mappings() [][]Mapping {
+	kind := 0
+	entry := 0
+	res := make([][]Mapping, 0)
+	res = append(res, make([]Mapping, 0))
+
+	for i := 3; i < len(input); i++ {
+		s := input[i]
+
+		if s == "" {
+			continue
+		}
+		if unicode.IsLetter(rune(s[0])) {
+			res = append(res, make([]Mapping, 0))
+			kind++
+			entry = 0
+			continue
+		}
+
+		fields := strings.Fields(s)
+		nums := readNums(fields)
+		if len(nums) == 0 {
+			fmt.Printf("%v: %q\n", i, s)
+		}
+		res[kind] = append(res[kind], newMap(nums))
+
+		entry++
+	}
+	return res
+}
+
+func readNums(s []string) []int {
+	numbers := make([]int, 0, len(s))
+	for _, s := range s {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+	return numbers
+}
+
+func newMap(arr []int) Mapping {
+	if len(arr) != 3 {
+		panic(fmt.Sprintf("invalid number of values in newMap. want %v; got %v", 3, len(arr)))
+	}
+
+	return Mapping{
+		DestinationRangeStart: arr[0],
+		SourceRangeStart:      arr[1],
+		RangeLength:           arr[2],
+	}
 }
