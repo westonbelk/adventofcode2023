ef7cd1599bcba25f446526ef4575f2ca71b5a165
Weston Belk <contact@westonbelk.com>
2023-12-06 00:18:07 -0800

    day6

--- day6/day6.go
+++ day6/day6.go
@@ -2,28 +2,43 @@ package day6
 
 import (
 	"fmt"
+	"strings"
+	"time"
+
+	"github.com/westonbelk/adventofcode/util"
 )
 
 func Execute() {
-	// start := time.Now()
-	// defer fmt.Println("time:", time.Since(start).Seconds())
-	// input := util.ReadLines("day6/input2.txt")
+	start := time.Now()
+	input := util.ReadLines("day6/input2.txt")
 	// input := util.ReadLines("day6/calibration.txt")
 
-	timeLimit := 40828492
-	distanceGoal := 233101111101487
+	times := util.ReadNums(strings.Fields(strings.Split(input[0], ":")[1]))
+	distances := util.ReadNums(strings.Fields(strings.Split(input[1], ":")[1]))
 
-	correctSolutions := 0
+	correctSolutions := make([]int, len(times))
 
-	for buttonTime := 0; buttonTime <= timeLimit; buttonTime += 1 {
-		raceTime := timeLimit - buttonTime
-		distance := raceTime * buttonTime
-		if distance > distanceGoal {
-			correctSolutions++
-			// fmt.Printf("Solution: hold button for %d seconds\n", buttonTime)
+	for i := range distances {
+		timeLimit := times[i]
+		distanceGoal := distances[i]
+
+		for buttonTime := 0; buttonTime <= timeLimit; buttonTime++ {
+			raceTime := timeLimit - buttonTime
+			speed := buttonTime
+			distance := raceTime * speed
+			if distance > distanceGoal {
+				correctSolutions[i] = correctSolutions[i] + 1
+			}
 		}
+		fmt.Println()
 	}
-	fmt.Println()
 
-	fmt.Println("total:", correctSolutions)
+	fmt.Println(correctSolutions)
+	total := 1
+	for _, solution := range correctSolutions {
+		total *= solution
+	}
+
+	fmt.Println("time:", time.Since(start).Seconds())
+	fmt.Println("total:", total)
 }
