33c0f0dfd01a14e9b76917ca1aa4fb1f3c48c409
Weston Belk <contact@westonbelk.com>
2023-12-08 22:45:38 -0800

    day 8 part 2

--- day8/day8.go
+++ day8/day8.go
@@ -2,6 +2,7 @@ package day8
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/westonbelk/adventofcode/util"
@@ -36,55 +37,48 @@ func Execute() {
 	}
 	fmt.Println(len(lookup))
 	fmt.Println("proceeding with walking")
+	fmt.Println()
 
+	nodes := []*Node{
+		lookup["MJA"],
+		lookup["RGA"],
+		lookup["JMA"],
+		lookup["XHA"],
+		lookup["DQA"],
+		lookup["AAA"],
+	}
+
+	answers := make([]string, len(nodes))
+	for i, node := range nodes {
+		answers[i] = strconv.Itoa(countSteps(node, steps))
+	}
+	fmt.Printf("LCM(%s)\n", strings.Join(answers, ", "))
+
+}
+
+func countSteps(node *Node, steps string) int {
 	var count int
 	found := false
-	node1 := lookup["MJA"] // start node
-	node2 := lookup["RGA"]
-	node3 := lookup["JMA"]
-	node4 := lookup["XHA"]
-	node5 := lookup["DQA"]
-	node6 := lookup["AAA"]
-	// node1 := lookup["11A"] // start node
-	// node2 := lookup["11A"]
-	// node3 := lookup["11A"]
-	// node4 := lookup["22A"]
-	// node5 := lookup["22A"]
-	// node6 := lookup["22A"]
-
 	for count = 0; !found; count++ {
 		i := (count) % len(steps)
 		r := steps[i]
 		switch r {
 		case 'R':
-			// fmt.Printf("%s [%s] => %s\n", node1.Value, string(r), node1.Right.Value)
-			// fmt.Printf("%s [%s] => %s\n", node4.Value, string(r), node4.Right.Value)
-			node1 = node1.Right
-			node2 = node2.Right
-			node3 = node3.Right
-			node4 = node4.Right
-			node5 = node5.Right
-			node6 = node6.Right
+			// fmt.Printf("%s [%s] => %s\n", node.Value, string(r), node.Right.Value)
+			node = node.Right
 		case 'L':
-			// fmt.Printf("%s [%s] => %s\n", node1.Value, string(r), node1.Left.Value)
-			// fmt.Printf("%s [%s] => %s\n", node4.Value, string(r), node4.Left.Value)
-			node1 = node1.Left
-			node2 = node2.Left
-			node3 = node3.Left
-			node4 = node4.Left
-			node5 = node5.Left
-			node6 = node6.Left
+			// fmt.Printf("%s [%s] => %s\n", node.Value, string(r), node.Left.Value)
+			node = node.Left
 		default:
 			panic(fmt.Sprintf("unhandled instruction: %v", r))
 		}
-		// fmt.Printf("%v", node1.Value[2] == 'Z')
+		// fmt.Printf("%v", node.Value[2] == 'Z')
 		// fmt.Println()
-		if node1.Value[2] == 'Z' && node2.Value[2] == 'Z' && node3.Value[2] == 'Z' && node4.Value[2] == 'Z' && node5.Value[2] == 'Z' && node6.Value[2] == 'Z' {
+		if node.Value[2] == 'Z' {
 			found = true
 		}
 	}
-
-	fmt.Println("number of steps: ", count)
+	return count
 }
 
 type Node struct {
