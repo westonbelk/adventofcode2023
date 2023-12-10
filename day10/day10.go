022d11fcfe1cd237c49bc7f758c833677ca14a9b
Weston Belk <contact@westonbelk.com>
2023-12-10 11:07:56 -0800

    day 10 part 2 beginning stuff

--- day10/day10.go
+++ day10/day10.go
@@ -3,15 +3,19 @@ package day10
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"image/png"
 	"os"
 	"strings"
 )
 
 var input []string
 
+var Red = color.RGBA64{65535, 0, 0, 65535}
+
 func Execute() {
 	inputBytes, err := os.ReadFile("day10/input.txt")
-	// inputBytes, err := os.ReadFile("day10/calibration2.txt")
+	// inputBytes, err := os.ReadFile("day10/calibration4.txt")
 	if err != nil {
 		fmt.Println("error reading file: exiting")
 		os.Exit(1)
@@ -21,8 +25,11 @@ func Execute() {
 	startLoc := findS(input)
 	player := Player{Location: startLoc, Direction: Down}
 	steps := 0
+	bounds := image.Rect(0, 0, len(input[0]), len(input))
+	img := image.NewRGBA64(bounds)
 
 	for steps == 0 || player.Location != startLoc {
+		img.SetRGBA64(player.Location.X, player.Location.Y, Red)
 		player.Advance()
 		steps++
 		fmt.Println()
@@ -31,6 +38,15 @@ func Execute() {
 		}
 	}
 
+	// draw image
+	f, err := os.Create("draw.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	png.Encode(f, img)
+
+	// part1 output
 	fmt.Println(strings.Join(input, "\n"))
 	fmt.Println("steps:", steps/2)
 }
