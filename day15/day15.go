524e15916d0bf14631ce07f847c1a006225b7257
Weston Belk <contact@westonbelk.com>
2023-12-16 14:16:00 -0800

    day 15 part 1

--- day15/day15.go
+++ day15/day15.go
@@ -1,5 +1,28 @@
 package day15
 
-func Execute() {
+import (
+	"fmt"
+	"strings"
 
+	"github.com/westonbelk/adventofcode/util"
+)
+
+func Hash(s string) int {
+	value := 0
+	for _, r := range s {
+		value += int(r)
+		value *= 17
+		value = value % 256
+	}
+	return value
+}
+
+func Execute() {
+	input := util.ReadLines("day15/input.txt")[0]
+
+	sum := 0
+	for _, e := range strings.Split(input, ",") {
+		sum += Hash(e)
+	}
+	fmt.Println("Sum:", sum)
 }
